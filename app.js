e3522b3d99053cd3b01b18e411d25067498113e8
Austin Sims <adsims2001@gmail.com>
2019-05-18 20:16:05 -0500

    draw recaman

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 import {draw, Side} from './draw.js';
+import {recaman} from './sequence.js';
 
-const arcs = [
-  {left: 0, right: 20, side: Side.TOP},
-  {left: 20, right: 40, side: Side.BOTTOM},
-  {left: 40, right: 60, side: Side.TOP},
-];
+const SCALE = 10;
+const LIMIT = 20;
+
+const seq = recaman(LIMIT);
+
+const arcs = [];
+for (let i = 0; i < seq.length-1; i++) {
+  const first = seq[i];
+  const second = seq[i+1];
+  const side = i % 2 === 0 ? Side.TOP : Side.BOTTOM;
+  let left, right;
+  if (first < second) {
+    left = first;
+    right = second;
+  } else if (first > second) {
+    left = second;
+    right = first;
+  } else {
+    throw new Error('Something went wrong, duplicate in sequence');
+  }
+  arcs.push({left: left * SCALE, right: right * SCALE, side});
+}
 
 draw(arcs);
