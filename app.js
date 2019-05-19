06f8ea676cb79d1467204cdba2420b9e5ead8211
Austin Sims <adsims2001@gmail.com>
2019-05-18 19:55:47 -0500

    separate draw into its own file

--- app.js
+++ app.js
@@ -1,44 +1,9 @@
-const WIDTH = 500;
-const HEIGHT = 300;
+import {draw, Side} from './draw.js';
 
-const canvas = document.createElement('canvas');
-canvas.id = 'c';
-canvas.width = WIDTH;
-canvas.height = HEIGHT;
-document.body.appendChild(canvas);
+const arcs = [
+  {left: 0, right: 20, side: Side.TOP},
+  {left: 20, right: 40, side: Side.BOTTOM},
+  {left: 40, right: 60, side: Side.TOP},
+];
 
-const stage = new createjs.Stage('c');
-stage.y = 150;
-
-const Side = {
-  TOP: 0,
-  BOTTOM: 1,
-};
-
-function makeArc({left, right, side}) {
-  if (!(right > left)) {
-    throw new Error('right must be greater than left');
-  }
-  if (!(side === Side.TOP || side === Side.BOTTOM)) {
-    throw new Error('side must be Side.TOP or Side.BOTTOM');
-  }
-  const radius = (right - left) / 2;
-  const midpoint = (right + left) / 2;
-  const arc = new createjs.Shape();
-  const graphics = arc.graphics.beginStroke('blue');
-  switch (side) {
-    case Side.TOP:
-      graphics.arc(midpoint, 0, radius, Math.PI, 2*Math.PI);
-      break;
-    case Side.BOTTOM:
-      graphics.arc(midpoint, 0, radius, 0, Math.PI);
-      break;
-  }
-  stage.addChild(arc);
-}
-
-makeArc({left: 0, right: 20, side: Side.TOP});
-makeArc({left: 20, right: 40, side: Side.BOTTOM});
-makeArc({left: 40, right: 60, side: Side.TOP});
-
-stage.update();
+draw(arcs);
---
+++ draw.js
@@ -0,0 +1,51 @@
+const WIDTH = 500;
+const HEIGHT = 300;
+const ID_CANVAS = 'c';
+
+export const Side = {
+  TOP: 0,
+  BOTTOM: 1,
+};
+
+export function draw(arcs) {
+  createCanvas();
+  const stage = createStage();
+
+  for (const arc of arcs) {
+    if (!(arc.right > arc.left)) {
+      throw new Error('right must be greater than left');
+    }
+    if (!(arc.side === Side.TOP || arc.side === Side.BOTTOM)) {
+      throw new Error('side must be Side.TOP or Side.BOTTOM');
+    }
+    const radius = (arc.right - arc.left) / 2;
+    const midpoint = (arc.right + arc.left) / 2;
+    const shape = new createjs.Shape();
+    const graphics = shape.graphics.beginStroke('blue');
+    switch (arc.side) {
+      case Side.TOP:
+        graphics.arc(midpoint, 0, radius, Math.PI, 2*Math.PI);
+        break;
+      case Side.BOTTOM:
+        graphics.arc(midpoint, 0, radius, 0, Math.PI);
+        break;
+    }
+    stage.addChild(shape);
+  }
+
+  stage.update();
+}
+
+function createCanvas() {
+  const canvas = document.createElement('canvas');
+  canvas.id = ID_CANVAS;
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+  document.body.appendChild(canvas);
+}
+
+function createStage() {
+  const stage = new createjs.Stage(ID_CANVAS);
+  stage.y = 150;
+  return stage;
+}
